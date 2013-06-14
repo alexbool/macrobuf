f1b3384c50d1b07d28ed2fe8d3df87d5ed0bc7a5
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-06-14 18:00:26 +0400

    Macros: base classes for macro parsers

---
+++ src/main/scala/me/alexbool/macrobuf/macros/MacroParserBase.scala
@@ -0,0 +1,29 @@
+package me.alexbool.macrobuf.macros
+
+import me.alexbool.macrobuf.Parser
+import java.io.InputStream
+import com.google.protobuf.CodedInputStream
+trait MacroParserBase[T] extends Parser[T] {
+  def parse(input: InputStream): T = {
+     val codedIn = CodedInputStream.newInstance(input)
+     parse(codedIn)
+  }
+
+  protected def parse(input: CodedInputStream): T
+}
+
+trait ListMacroParserBase[T] extends Parser[Seq[T]] {
+  def parse(input: InputStream): Seq[T] = {
+    val codedIn = CodedInputStream.newInstance(input)
+    val buffer = collection.mutable.ListBuffer[T]()
+    while (!codedIn.isAtEnd) {
+      val size = codedIn.readRawVarint32()
+      val oldLimit = codedIn.pushLimit(size)
+      buffer += parse(codedIn)
+      codedIn.popLimit(oldLimit)
+    }
+    buffer.to[Seq]
+  }
+
+  protected def parse(input: CodedInputStream): T
+}
