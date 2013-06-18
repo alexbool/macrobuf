dc3fe1ce7ac2626783c0be56d89644556b17c476
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-06-18 23:01:07 +0400

    More WIP

--- src/main/scala/me/alexbool/macrobuf/macros/ParserHelper.scala
+++ src/main/scala/me/alexbool/macrobuf/macros/ParserHelper.scala
@@ -2,23 +2,24 @@ package me.alexbool.macrobuf.macros
 
 import scala.reflect.macros.Context
 import me.alexbool.macrobuf.MessageMetadata
-import com.google.protobuf.CodedInputStream
+import com.google.protobuf.{WireFormat, CodedInputStream}
 
 class ParserHelper[C <: Context](val c: C) {
 
   import c.universe._
+  import WireFormat._
 
   val mm = MessageMetadata[c.universe.type](c.universe)
 
-  def readPrimitive(tpe: c.Type)(in: c.Expr[CodedInputStream]): c.Expr[Any] = {
+  def parsePrimitive(tpe: c.Type)(tag: c.Expr[Int], in: c.Expr[CodedInputStream]): c.Expr[Any] = {
     import c.universe.definitions._
-    if      (tpe =:= IntTpe)         readInt(in)
-    else if (tpe =:= LongTpe)        readLong(in)
-    else if (tpe =:= ShortTpe)       readShort(in)
-    else if (tpe =:= BooleanTpe)     readBoolean(in)
-    else if (tpe =:= FloatTpe)       readFloat(in)
-    else if (tpe =:= DoubleTpe)      readDouble(in)
-    else if (tpe =:= typeOf[String]) readString(in)
+    if      (tpe =:= IntTpe)         reify { requireWireFormat(tag, WIRETYPE_VARINT).splice;           readInt(in).splice     }
+    else if (tpe =:= LongTpe)        reify { requireWireFormat(tag, WIRETYPE_VARINT).splice;           readLong(in).splice    }
+    else if (tpe =:= ShortTpe)       reify { requireWireFormat(tag, WIRETYPE_VARINT).splice;           readShort(in).splice   }
+    else if (tpe =:= BooleanTpe)     reify { requireWireFormat(tag, WIRETYPE_VARINT).splice;           readBoolean(in).splice }
+    else if (tpe =:= FloatTpe)       reify { requireWireFormat(tag, WIRETYPE_FIXED32).splice;          readFloat(in).splice   }
+    else if (tpe =:= DoubleTpe)      reify { requireWireFormat(tag, WIRETYPE_FIXED64).splice;          readDouble(in).splice  }
+    else if (tpe =:= typeOf[String]) reify { requireWireFormat(tag, WIRETYPE_LENGTH_DELIMITED).splice; readString(in).splice  }
     else throw new IllegalArgumentException("Unsupported primitive type")
   }
 
@@ -30,6 +31,11 @@ class ParserHelper[C <: Context](val c: C) {
   def readFloat(in: c.Expr[CodedInputStream]): c.Expr[Float]     = reify { in.splice.readFloat() }
   def readDouble(in: c.Expr[CodedInputStream]): c.Expr[Double]   = reify { in.splice.readDouble() }
   def readString(in: c.Expr[CodedInputStream]): c.Expr[String]   = reify { in.splice.readString() }
+
+  def requireWireFormat(tag: c.Expr[Int], wf: Int): c.Expr[Unit] = reify {
+    val actualWf = tag.splice & (1 << 3) - 1 // XXX Kinda hack
+    require(actualWf == wf, s"Wrong field wire format: found $actualWf, expected $wf")
+  }
 
   def parseField(f: mm.Field, in: c.Expr[CodedInputStream]): c.Expr[Any] = {
     ???
@@ -65,12 +71,13 @@ class ParserHelper[C <: Context](val c: C) {
     m.fields.map(f => (f, varDefForField(f))).toMap
   }
 
-  private def patternMatchOnFieldNumber(varDefs: Map[mm.Field, ValDef]): Match = {
+  private def patternMatchOnFieldNumber(varDefs: Map[mm.Field, ValDef], in: c.Expr[CodedInputStream]): Match = {
     val cases: List[CaseDef] = varDefs.to[List].map(fieldAndVar => {
-      CaseDef(Literal(Constant(fieldAndVar._1.number)), EmptyTree, readField(fieldAndVar._1, fieldAndVar._2))
+      CaseDef(Literal(Constant(fieldAndVar._1.number)), EmptyTree, readField(fieldAndVar._1, fieldAndVar._2, in))
     })
     Match(Ident(newTermName("number")), cases)
   }
 
-  private def readField(f: mm.Field, readIntoVar: ValDef): Tree = ???
+  private def readField(f: mm.Field, readIntoVar: ValDef, in: c.Expr[CodedInputStream]): Tree =
+    Assign(readIntoVar, parseField(f, in).tree)
 }
