f1fd2b9e87862d235e136cf53ece8148e7a176c0
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-06-25 16:10:41 +0400

    scalastyle: do not check class names in annotations

--- src/main/scala/me/alexbool/macrobuf/annotation/annotations.scala
+++ src/main/scala/me/alexbool/macrobuf/annotation/annotations.scala
@@ -3,5 +3,9 @@ package me.alexbool.macrobuf.annotation
 import scala.annotation.StaticAnnotation
 import scala.annotation.meta.getter
 
+// scalastyle:off class.name
+
 @getter
 final class packed(val value: Boolean = true) extends StaticAnnotation
+
+// scalastyle:on class.name
